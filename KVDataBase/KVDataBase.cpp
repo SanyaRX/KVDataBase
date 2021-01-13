b4ca7f8957e00ae62aa87e4eed70fa15a77f7667
VladimirMihalyuk <vladimir.mihalyuk@gmail.com>
2021-01-13 21:44:29 +0300

    C}{C

--- KVDataBase/KVDataBase.cpp
+++ KVDataBase/KVDataBase.cpp
@@ -1,21 +1,8 @@
-#include <iostream>
-#include <filesystem>
-#include <string>
-#include <fstream>
-#include <vector>
-#include "windows.h"
+#include "KVDataBase.h"
 
-using namespace std;
-namespace fs = std::filesystem;
+Database:: Database() {}
 
-string dots = "..";
-string doubleslash = "\\";
-string dottxt = ".txt";
-string metasymbol = ",";
-char token = ',';
-
-//private
-void eraseSubStr(std::string& mainStr, const std::string& toErase) {
+void Database::eraseSubStr(std::string& mainStr, const std::string& toErase) {
 	size_t pos = mainStr.find(toErase);
 	if (pos != std::string::npos) {
 		mainStr.erase(pos, toErase.length());
@@ -23,17 +10,17 @@ void eraseSubStr(std::string& mainStr, const std::string& toErase) {
 }
 
 //private
-bool createDirectory(string path) {
+bool Database::createDirectory(string path) {
 	return CreateDirectory(wstring(path.begin(), path.end()).c_str(), NULL);
 }
 
 //private
-bool createTable(string table_name) {
+bool Database::createTable(string table_name) {
 	return createDirectory(dots + doubleslash + table_name);
 }
 
 //private
-string concatKeys(vector<string> keys) {
+string Database::concatKeys(vector<string> keys) {
 	string key = "";
 
 	for (auto p : keys) {
@@ -43,11 +30,11 @@ string concatKeys(vector<string> keys) {
 }
 
 //private
-void addValue(string table_name, string key, string value) {
+void Database::addValue(string table_name, string key, string value) {
 	string path = dots + doubleslash + table_name + doubleslash + key;
 	string last = "0";
 	createDirectory(path);
-	
+
 	for (auto& p : fs::directory_iterator(path))
 		last = p.path().u8string();
 
@@ -55,7 +42,7 @@ void addValue(string table_name, string key, string value) {
 	eraseSubStr(last, dottxt);
 
 	int current = stoi(last) + 1;
-	
+
 	ofstream myfile(path + doubleslash + to_string(current) + dottxt);
 	if (myfile.is_open()) {
 		myfile << value;
@@ -64,23 +51,23 @@ void addValue(string table_name, string key, string value) {
 }
 
 //private
-void deleteAllValuesByKey(string table_name, string key) {
+void Database::deleteAllValuesByKey(string table_name, string key) {
 	fs::remove_all(dots + doubleslash + table_name + doubleslash);
 }
 
 //private
-void deleteValueByKey(string table_name, string key) {
+void Database::deleteValueByKey(string table_name, string key) {
 	string path = dots + doubleslash + table_name + doubleslash + key;
 
 	for (auto& p : fs::directory_iterator(path)) {
 		string s = p.path().u8string();
 		DeleteFile(wstring(s.begin(), s.end()).c_str());
 		return;
-	}	
+	}
 }
 
 //private
-vector<string> getByKey(string table_name, string key) {
+vector<string> Database::getByKey(string table_name, string key) {
 	string path = dots + doubleslash + table_name + doubleslash + key;
 	vector<string> vector;
 
@@ -100,7 +87,7 @@ vector<string> getByKey(string table_name, string key) {
 }
 
 //private
-void updateByKey(string table_name, string key, string value) {
+void Database::updateByKey(string table_name, string key, string value) {
 	string path = dots + doubleslash + table_name + doubleslash + key;
 	for (auto& p : fs::directory_iterator(path)) {
 		ifstream myfile(p.path().u8string());
@@ -117,7 +104,7 @@ void updateByKey(string table_name, string key, string value) {
 }
 
 //private 
-vector<string> split(const char* str) {
+vector<string> Database::split(const char* str) {
 	vector<string> result;
 
 	do {
@@ -133,7 +120,7 @@ vector<string> split(const char* str) {
 }
 
 //private 
-int find(vector<string> vector, string find) {
+int Database::find(vector<string> vector, string find) {
 	for (int i = 0; i < vector.size(); i++) {
 		if (find == vector[i])
 			return i;
@@ -141,9 +128,9 @@ int find(vector<string> vector, string find) {
 	return 0;
 }
 
-bool createTableWithKeys(string table_name, vector<string> keys) {
+bool Database::createTableWithKeys(string table_name, vector<string> keys) {
 	ofstream myfile(dots + doubleslash + table_name + dottxt);
-	if (myfile.is_open()) {		
+	if (myfile.is_open()) {
 		for (auto p : keys) {
 			myfile << p << endl;
 		}
@@ -153,23 +140,23 @@ bool createTableWithKeys(string table_name, vector<string> keys) {
 	return false;
 }
 
-bool deleteTable(string table_name) {
+bool Database::deleteTable(string table_name) {
 	return fs::remove_all(dots + doubleslash + table_name);
 }
 
-void addValueByKeys(string table_name, vector<string> keys, string value) {
+void Database::addValueByKeys(string table_name, vector<string> keys, string value) {
 	addValue(table_name, concatKeys(keys), value);
 }
 
-void deleteAllValuesByKeys(string table_name, vector<string> keys) {
+void Database::deleteAllValuesByKeys(string table_name, vector<string> keys) {
 	deleteAllValuesByKey(table_name, concatKeys(keys));
 }
 
-void deleteValueByKeys(string table_name, vector<string> keys) {
+void Database::deleteValueByKeys(string table_name, vector<string> keys) {
 	deleteValueByKey(table_name, concatKeys(keys));
 }
 
-string getSortedByKey(string table_name, string key, bool isFirst) {
+string Database::getSortedByKey(string table_name, string key, bool isFirst) {
 	vector<string> vector;
 
 	ifstream myfile(dots + doubleslash + table_name + dottxt);
@@ -178,13 +165,13 @@ string getSortedByKey(string table_name, string key, bool isFirst) {
 		while (getline(myfile, line)) {
 			vector.push_back(line);
 		}
-		myfile.close();	
+		myfile.close();
 	}
 	int index = find(vector, key);
 	string sortedString = "";
 	string sortedPath = "";
 	string temp;
-	
+
 	for (auto& p : fs::directory_iterator(dots + doubleslash + table_name)) {
 		vector = split(p.path().u8string().c_str());
 		temp = vector[index];
@@ -211,24 +198,10 @@ string getSortedByKey(string table_name, string key, bool isFirst) {
 	return "";
 }
 
-vector<string> getByKeys(string table_name, vector<string> keys) {
+vector<string> Database::getByKeys(string table_name, vector<string> keys) {
 	return getByKey(table_name, concatKeys(keys));
 }
 
-void updateByKeys(string table_name, vector<string> key, string value) {
+void Database::updateByKeys(string table_name, vector<string> key, string value) {
 	updateByKey(table_name, concatKeys(key), value);
 }
-
-int main() {
-	vector<string> vector;
-	vector.push_back("a2");
-	vector.push_back("b2");
-	/*getByKeys("table_name", vector);*/
-
-	for (auto p : getByKeys("table_name", vector)) {
-		cout << p << endl;
-	}
-
-	
-}
-
---
+++ KVDataBase/KVDataBase.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <iostream>
+#include <filesystem>
+#include <string>
+#include <fstream>
+#include <vector>
+#include "windows.h"
+
+using namespace std;
+namespace fs = std::filesystem;
+
+class Database
+{
+public:
+	Database();
+	bool createTableWithKeys(string table_name, vector<string> keys);
+	bool deleteTable(string table_name);
+	void addValueByKeys(string table_name, vector<string> keys, string value);
+	void deleteAllValuesByKeys(string table_name, vector<string> keys);
+	void deleteValueByKeys(string table_name, vector<string> keys);
+	string getSortedByKey(string table_name, string key, bool isFirst);
+	vector<string> getByKeys(string table_name, vector<string> keys);
+	void updateByKeys(string table_name, vector<string> key, string value);
+
+private:
+	string dots = "..";
+	string doubleslash = "\\";
+	string dottxt = ".txt";
+	string metasymbol = ",";
+	char token = ',';
+
+	void eraseSubStr(std::string& mainStr, const std::string& toErase);
+	bool createDirectory(string path);
+	bool createTable(string table_name);
+	string concatKeys(vector<string> keys);
+	void addValue(string table_name, string key, string value);
+	void deleteAllValuesByKey(string table_name, string key);
+	void deleteValueByKey(string table_name, string key);
+	vector<string> getByKey(string table_name, string key);
+	void updateByKey(string table_name, string key, string value);
+	vector<string> split(const char* str);
+	int find(vector<string> vector, string find);
+};
